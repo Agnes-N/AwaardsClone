4ce9080f7bf22f2c0e6ed7d933bd775602f14aae
agnes <aggyreina@gmail.com>
2019-10-25 09:28:15 +0200

    creates project model

--- post/models.py
+++ post/models.py
@@ -8,4 +8,10 @@ class Profile(models.Model):
     bio = models.CharField(max_length =300)
     contact = models.CharField(max_length =30)    
 
-
+class Project(models.Model):
+    title = models.CharField(max_length =30)
+    project_image = models.ImageField(upload_to = 'landing_images/', null=True)
+    description = models.CharField(max_length =300)
+    # link = 
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    # date = models.DateTimeField(auto_now_add=True)
